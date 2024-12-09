c769d77bb3921fc953c6a51a68e94256eaf74999
santo <rabiul.islam@bracits.com>
2024-12-09 18:52:56 +0600

    Initial Commit

---
+++ src/main/webapp/app/feedback-request-form/feedback-request-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FeedbackRequestFormComponent } from './feedback-request-form.component';
+
+describe('FeedbackRequestFormComponent', () => {
+  let component: FeedbackRequestFormComponent;
+  let fixture: ComponentFixture<FeedbackRequestFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FeedbackRequestFormComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(FeedbackRequestFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/main/webapp/app/feedback-request-form/feedback-request-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-feedback-request-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './feedback-request-form.component.html',
+  styleUrl: './feedback-request-form.component.scss'
+})
+export class FeedbackRequestFormComponent {
+
+}
